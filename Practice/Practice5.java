266626d1d5e4cad1befed707176e8fbd612a7528
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-09 21:43:38 +0530

    Commit

--- Practice/Practice5.java
+++ Practice/Practice5.java
@@ -5,11 +5,20 @@ public class Practice5 {
         // Practice 1 ----->
         // int n=4;
         // for(int i=n;i>0;i--){
-        //     for(int j=0;j<i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.print("\n");
+        // for(int j=0;j<i;j++){
+        // System.out.print("*");
         // }
-        
+        // System.out.print("\n");
+        // }
+
+        // Practice 2-------->
+
+        int n1 = 5;
+        int k = 0;
+        while (k <= n1) {
+            k = k + (k + 1);
+            k++;
+        }
+        System.out.println("Ans. : " + k);
     }
 }
