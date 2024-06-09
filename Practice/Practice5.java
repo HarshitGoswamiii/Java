be51f82ad557f7aa6697c03f969a3efc855d4bc5
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-09 17:54:10 +0530

    commit

--- Practice/Practice5.java
+++ Practice/Practice5.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Practice5 {
     public static void main(String[] args) {
-        int n=4;
-        for(int i=n;i>0;i--){
-            for(int j=0;j<i;j++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
+
+        // Practice 1 ----->
+        // int n=4;
+        // for(int i=n;i>0;i--){
+        //     for(int j=0;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // Practice 2 ------->
+        int w=0;
+        Scanner s =new Scanner(System.in);
+        int d= s.nextInt();
+        while(w<=d){
+            
         }
     }
 }
