44989a724833247cff2d84578ca49498a08b5374
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-10 17:49:05 +0530

    commit

--- Tutorials/java07.java
+++ Tutorials/java07.java
@@ -1,6 +1,6 @@
 public class java07 {
     public static void main(String[] args) {
         int[] arr={10,20,30,40,50};
-        System.out.println(arr);
+        System.out.println(arr[1]);
     }
 }
