75d43570ad35e9602a476e5acc142927205bd6fd
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-10 18:29:03 +0530

    Commit

--- Tutorials/java07_array.java
+++ Tutorials/java07_array.java
@@ -3,6 +3,9 @@ public class java07_array {
         // int[] arr={10,20,30,40,50};
         
         int[] arr={10,20,30,40,50};
+        for(int i = arr.length-1; i>=0;i--){
+            System.out.println(arr[i]);
+        }
 
 
         // for(int element : arr){
@@ -14,7 +17,7 @@ public class java07_array {
         //     System.out.println(arr[i]);
         // }
 
-        
+
         // arr = new int[5];
         // arr[0]=5;
         // arr[1]=50;
