02427664ece9b873433519c8283c7f7924a09d92
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-10 19:41:30 +0530

    Commit

--- Practice/Practice6.java
+++ Practice/Practice6.java
@@ -1,5 +1,24 @@
+
+import java.util.Scanner;
+
 public class Practice6 {
     public static void main(String[] args) {
         
+        // Practice 1 ------->
+
+        // float[] arr = {10.5f,19.7f,33.4f,12.4f};
+        // float sum=0;
+        // for(int i =0; i < arr.length; i++){
+        //     sum= sum+arr[i];
+        //     // System.out.println(sum);
+        // }
+        // System.out.println(sum);
+
+        //  Practice 2 ----->
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = s.nextInt();
+        int[] arr = {10,20,30,40};
     }
 }
