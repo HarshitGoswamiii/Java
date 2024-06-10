d9bb7d2f21fc30d4a96b4488806f1f442429459c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-10 19:10:04 +0530

    commit

--- Tutorials/java07_m_array.java
+++ Tutorials/java07_m_array.java
@@ -2,15 +2,21 @@ public class java07_m_array {
     public static void main(String[] args) {
         
         // int[] arr;
-        int[][] flat;
-        flat=new int[2][3];
-        flat[0][0]=100;
-        flat[0][1]=101;
-        flat[0][2]=102;
+        int[][][] flat;
+        flat=new int[2][3][3];
+        flat[0][0][0]=100;
+        flat[0][0][1]=101;
+        flat[0][0][2]=102;
+        flat[0][0][0]=100;
+        flat[0][0][1]=101;
+        flat[0][0][2]=102;
+        flat[0][0][0]=100;
+        flat[0][0][1]=101;
+        flat[0][0][2]=102;
         flat[1][0]=201;
         flat[1][1]=202;
         flat[1][2]=203;
-        for(int i=0; i<flat.length;i++){
+        for(int i=0; i<flat.length ; i++){
             for(int j =0 ; j < flat[i].length; j++){
                 System.out.print(flat[i][j]);
                 System.out.print(" ");
