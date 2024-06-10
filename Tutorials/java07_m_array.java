62c0700bbe125e5d9081381ffc49f7daa0cccd95
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-10 19:13:53 +0530

    commit

--- Tutorials/java07_m_array.java
+++ Tutorials/java07_m_array.java
@@ -1,25 +1,42 @@
 public class java07_m_array {
     public static void main(String[] args) {
-        
+
         // int[] arr;
         int[][][] flat;
-        flat=new int[2][3][3];
-        flat[0][0][0]=100;
-        flat[0][0][1]=101;
-        flat[0][0][2]=102;
-        flat[0][0][0]=100;
-        flat[0][0][1]=101;
-        flat[0][0][2]=102;
-        flat[0][0][0]=100;
-        flat[0][0][1]=101;
-        flat[0][0][2]=102;
-        flat[1][0]=201;
-        flat[1][1]=202;
-        flat[1][2]=203;
-        for(int i=0; i<flat.length ; i++){
-            for(int j =0 ; j < flat[i].length; j++){
-                System.out.print(flat[i][j]);
-                System.out.print(" ");
+        flat = new int[2][3][3];
+        flat[0][0][0] = 100;
+        flat[0][0][1] = 101;
+        flat[0][0][2] = 102;
+        flat[0][1][0] = 200;
+        flat[0][1][1] = 201;
+        flat[0][1][2] = 202;
+        flat[0][2][0] = 100;
+        flat[0][2][1] = 101;
+        flat[0][2][2] = 102;       
+             flat[0][0][0] = 100;
+        flat[0][0][1] = 101;
+        flat[0][0][2] = 102;
+        flat[0][1][0] = 200;
+        flat[0][1][1] = 201;
+        flat[0][1][2] = 202;
+        flat[0][2][0] = 100;
+        flat[0][2][1] = 101;
+        flat[0][2][2] = 102;        
+            flat[0][0][0] = 100;
+        flat[0][0][1] = 101;
+        flat[0][0][2] = 102;
+        flat[0][1][0] = 200;
+        flat[0][1][1] = 201;
+        flat[0][1][2] = 202;
+        flat[0][2][0] = 100;
+        flat[0][2][1] = 101;
+        flat[0][2][2] = 102;
+        for (int i = 0; i < flat.length; i++) {
+            for (int j = 0; j < flat[i].length; j++) {
+                for (int k = 0; k = flat[j][k]; k++) {
+                    System.out.print(flat[i][j][k]);
+                    System.out.print(" ");
+                }
             }
             System.out.println("");
         }
