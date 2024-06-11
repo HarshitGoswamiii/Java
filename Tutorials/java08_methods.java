277863da1d8548d46187313ed120483284894ce1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-11 18:06:42 +0530

    Commit

--- Tutorials/java08_methods.java
+++ Tutorials/java08_methods.java
@@ -4,11 +4,19 @@ public class java08_methods {
     z=x+y;
     return z;
     }
+    static int diff(int x, int y){
+        int z;
+        z=x-y;
+        return z;
+        }
     public static void main(String[] args){
         int a=10;
         int b=12;
         int c;
+        int c1;
         c=sum(a,b);
         System.out.println("Addition : "+c);
+        c1=diff(a,b);
+        System.out.println("Addition : "+c1);
     }
 }
