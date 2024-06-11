c0e596bc358da9c1a30cb46494d6840f59221328
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-11 18:03:26 +0530

    commit

--- Tutorials/java08_methods.java
+++ Tutorials/java08_methods.java
@@ -1,5 +1,14 @@
 public class java08_methods {
+    static int sum(int x, int y){
+    int z;
+    z=x+y;
+    return z;
+    }
     public static void main(String[] args){
-        
+        int a=10;
+        int b=12;
+        int c;
+        c=sum(a,b);
+        System.out.println("Addition : "+c);
     }
 }
