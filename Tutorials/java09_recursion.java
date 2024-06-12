62c52560d467c022d65dbf815b032f0b9d5b84d6
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-12 17:55:56 +0530

    Commit

--- Tutorials/java09_recursion.java
+++ Tutorials/java09_recursion.java
@@ -1,18 +1,26 @@
 public class java09_recursion {
-    static int factorial(int n){
-        if(n==0 || n==1){
-            return 1;
-        }
-        else{
 
-            int result;
-            result = n*factorial(n-1);
-            return result;
-        }
-    }
-    public static void main(String[] args) {
-        int x=5;
-        System.out.println("The factorial of "+x+" : "+factorial(x));
+    // Fibinacci Series ----------->
+
+    
+
+
+    // Factorial using recursion ---------->
+
+    // static int factorial(int n){
+    //     if(n==0 || n==1){
+    //         return 1;
+    //     }
+    //     else{
+
+    //         int result;
+    //         result = n*factorial(n-1);
+    //         return result;
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     int x=5;
+    //     System.out.println("The factorial of "+x+" : "+factorial(x));
         
-    }
+    // }
 }
