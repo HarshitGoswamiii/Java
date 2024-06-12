40b4105c7aff92b8403b2cbb68a4037cb49a67ae
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-12 18:30:25 +0530

    Commit

--- Practice/Practice7.java
+++ Practice/Practice7.java
@@ -1,11 +1,26 @@
 public class Practice7 {
     // Practice 1 ------->
-    static void table(int a){
-        for(int i =1; i<=10;i++){
-            System.out.println(a*i);
+
+    // static void table(int a){
+    //     for(int i =1; i<=10;i++){
+    //         System.out.println(a*i);
+    //     }
+    // }
+    // public static void main(String[] args){
+    //     table(2);
+    // }
+
+    // Practice 2 ------>
+     static void pattern(int a){
+        for(int i=0; i<a;i++)
+        {
+            for(int j=0; j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
-    }
-    public static void main(String[] args){
-        table(2);
-    }
+     }
+     public static void main(String[] args) {
+         pattern(4);
+     }
 }
