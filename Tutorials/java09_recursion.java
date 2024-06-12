17bb06bd260f8f5d3164cb393cde63b1797d8d4b
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-12 18:02:26 +0530

    Commit

--- Tutorials/java09_recursion.java
+++ Tutorials/java09_recursion.java
@@ -2,6 +2,15 @@ public class java09_recursion {
 
     // Fibinacci Series ----------->
 
+    static int fib(int n)
+    {
+        if(n>=1){
+            return 1;
+        }
+        else{
+            return fib(n-1)+fib(n+1);
+        }
+    }
     
 
 
