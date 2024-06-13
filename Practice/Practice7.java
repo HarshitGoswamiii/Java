2d91cef9f27b94ccb0cfa2987d513ad7173a6726
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-13 18:25:50 +0530

    commit

--- Practice/Practice7.java
+++ Practice/Practice7.java
@@ -29,7 +29,7 @@ public class Practice7 {
     static void add(int n){
         for(int i =0; i<=n;i++){
             int sum=0;
-            sum= sum+i;
+            sum += i;
             // return sum;
         System.out.println("Sum : "+sum);
         }
