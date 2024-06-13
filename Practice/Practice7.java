154492979da6cc7d97579da0a7bb6db34106ce4f
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-13 18:58:27 +0530

    commit

--- Practice/Practice7.java
+++ Practice/Practice7.java
@@ -50,4 +50,21 @@ public class Practice7 {
     // public static void main(String[] args) {
     //     pattern(4);
     // }
-}}
+
+    // Practice 5 -------->
+
+        static int  fib(int n){
+            if(n==1){
+                return 0;
+            }
+            else if(n==2){
+                return 1;
+            }
+            else{
+                return fib(n-1) + fib(n-2);
+            }}
+    public static void main(String[] args){
+
+        System.out.println("Fibonacci Series "+ fib(7));
+
+        }}
