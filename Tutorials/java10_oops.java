fc25b96c02bfa94ba47e0a3123168263ddc14fa1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-15 22:05:09 +0530

    Commit

--- Tutorials/java10_oops.java
+++ Tutorials/java10_oops.java
@@ -1,5 +1,20 @@
 public class java10_oops {
+
+    class Person{
+        String name;
+        int age;
+        int height;
+        public void getage(){
+            System.out.println("Age : " + age);
+        }
+
+    }
     public static void main(String[] args) {
+        Person Harshit = new Person();
+        Harshit.age = 18;
+        Harshit.height= 6;
+        Harshit.name="Harshit Goswami";
+
         
     }
 }
