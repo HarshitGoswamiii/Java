f95cbe4207921d27c1c76caca74a50231bd41362
Harshit Goswami <harshitgoswami244@gmail.com>
2024-06-15 22:15:51 +0530

    commit

--- Tutorials/java10_oops.java
+++ Tutorials/java10_oops.java
@@ -1,27 +1,56 @@
 public class java10_oops {
+    public class Java10Oops {
 
-    class Person {
-        String name;
-        int age;
-        int height;
-
-        public void getdetails() {
-            System.out.println("Age : " + age);
-            System.out.println("name : " + name);
-            System.out.println("Height : " + height);
+        static class Person {
+            String name;
+            int age;
+            int height;
+    
+            public void getDetails() {
+                System.out.println("Age: " + age);
+                System.out.println("Name: " + name);
+                System.out.println("Height: " + height);
+            }
         }
+    
+        public static void main(String[] args) {
+            Person harshit = new Person();
+            harshit.age = 18;
+            harshit.height = 6;
+            harshit.name = "Harshit Goswami";
+    
+            Person manjeet = new Person();
+            manjeet.age = 18;
+            manjeet.height = 6;
+            manjeet.name = "Manjeet";
+    
+            harshit.getDetails(); // Call the method to print details
+        }
+    }
+    
 
+    // class Person {
+    //     String name;
+    //     int age;
+    //     int height;
+
+    //     public void getdetails() {
+    //         System.out.println("Age : " + age);
+    //         System.out.println("name : " + name);
+    //         System.out.println("Height : " + height);
+    //     }
+
+    // }
+
+    // public static void main(String[] args) {
+    //     Person harshit = new Person();
+    //     harshitarshit.age = 18;
+    //     harshitarshit.height = 6;
+    //     harshitarshit.name = "Harshit Goswami";
+    //     Person manjeet = new Person();
+    //     manjeet.age = 18;
+    //     manjeet.height = 6;
+    //     manjeet.name = "Manjeet";
+    //     harshit.getdetails();
     }
 
-    public static void main(String[] args) {
-        Person harshit = new Person();
-        harshitarshit.age = 18;
-        harshitarshit.height = 6;
-        harshitarshit.name = "Harshit Goswami";
-        Person manjeet = new Person();
-        manjeet.age = 18;
-        manjeet.height = 6;
-        manjeet.name = "Manjeet";
-        harshit.getdetails();
-    }
-}
