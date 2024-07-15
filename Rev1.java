edd889dcd51b4608b50e342375e04dce7cde1b6d
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-15 17:30:33 +0530

    Commit

--- Rev1.java
+++ Rev1.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 public class Rev1 {
     public static void main(String[] args) {
       Scanner s = new Scanner(System.in);  
-      System.out.println("Enter the value of A : ");
+      System.out.print("Enter the Marks of English : ");
       int a = s.nextInt();
-      System.out.println("Enter the value of B : ");
+      System.out.print("Enter the Marks of Hindi : ");
       int b = s.nextInt();
-      int sum = a+b;
+      System.out.print("Enter the Marks of Maths : ");
+      int c = s.nextInt();
+      System.out.print("Enter the Marks of Social Science : ");
+      int d = s.nextInt();
+      System.out.print("Enter the Marks of Science : ");
+      int e = s.nextInt();
+      int sum = a+b+c+d+e;
       System.out.println("Sum : "+sum);
     }
 }
