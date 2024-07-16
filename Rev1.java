a90c80a0a4944549f360f9cbe21bf11717bf0f90
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-16 13:02:44 +0530

    cimmt

--- Rev1.java
+++ Rev1.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-public class Rev1 {
+public class rev1 {
     public static void main(String[] args) {
       Scanner s = new Scanner(System.in);  
       System.out.print("Enter the Marks of English : ");
