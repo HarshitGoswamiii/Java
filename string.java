58125dfc73248d3c15036ed48fed434cad3af5a5
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-16 19:20:34 +0530

    Commit

---
+++ string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String a = "Happy Birthday";
+        String a1 = a.toLowerCase();
+        String a2 = a.toUpperCase();
+        System.out.println("Lowercase : "+ a1);
+        System.out.println("Lowercase : "+ a2);  
+    }
+}
