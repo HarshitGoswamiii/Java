35b55f7ee28cbade940b0f8c71faab5639127cac
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-18 17:40:18 +0530

    commit

--- loop.java
+++ loop.java
@@ -1,11 +1,11 @@
 public class loop {
     public static void main(String[] args){
 
-        int i = 5;
-        do { 
-            System.out.println(i);
-            i--;
-        } while (i>=1);
+        // int i = 5;
+        // do { 
+        //     System.out.println(i);
+        //     i--;
+        // } while (i>=1);
 
         // int a=110;
         // while(a>=100){
