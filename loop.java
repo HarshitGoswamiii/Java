4519ede4e38147928c6779bc649b480dbd52b8a2
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-18 18:04:44 +0530

    commit

--- loop.java
+++ loop.java
@@ -1,10 +1,20 @@
 public class loop {
     public static void main(String[] args){
 
-        for(int i=0;i<4;i++){
-
+        for(int i=0;i<=4;i++){
+            for(int k =0;k<i;k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
         }
         
+        for(int a=4; a>0; a--){
+            for(int f=0; f<a;f++){
+
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
         // int n=5;
         // for (int i = 1; i <= n; i++) {
         //     System.out.println(2*i+1);     
