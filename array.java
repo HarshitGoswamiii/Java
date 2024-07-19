0583ba9c74f4059e797341b58045506c2be0ba6a
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-19 20:53:12 +0530

    commit

---
+++ array.java
@@ -0,0 +1,29 @@
+public class array {
+    public static void main(String[] args) {
+        // int[] arr={98,76,87,76};
+    //     float sum=0;
+    //     for(float element:arr){
+    //         // System.out.println(element);
+    //         sum +=element;
+    //     }
+    //     System.out.println(sum);
+
+    // int a=70;
+    // boolean avail=false;
+    // for(int element:arr){
+    //     if(a==element){
+    //         avail=true;
+    //         break;
+    //     }     
+    //     }
+    // if(avail){
+    //     System.out.println("Present");
+    //     }
+    // else{
+    //     System.out.println("Not Present");
+    //     }
+
+    int[] physics={70,80,87,78};
+    for(int element)
+    }
+}
