eec9dda3a5c50766ca8e748a4315cc624a942f23
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-19 18:54:22 +0530

    comit

--- loop.java
+++ loop.java
@@ -1,7 +1,10 @@
 public class loop {
     public static void main(String[] args){
 
-        
+        int a=8;
+        for(int i=0; i<=10;i++){
+            System.out.println(a+"*"+i+"="+(a*i));
+        }
 
         // int i=1;
         // int rows=5;
