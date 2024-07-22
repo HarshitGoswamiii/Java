cede7ba21cd4168ed42450d4ba3b7522162ed061
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-22 20:32:53 +0530

    commit

--- array2.java
+++ array2.java
@@ -2,16 +2,29 @@ public class array2 {
     public static void main(String[] args) {
       
         int[] red = {1,2,3,4,5};
-        int l = red.length;
-        int temp;
-        int n = Math.floorDiv(l, 2);
-        for(int i =0; i<=n;i++){
-            temp = red[i];
-            red[i]= red[l-i-1];
-            red[l-i-1] = temp;
+        // int l = red.length;
+        // int temp;
+        // int n = Math.floorDiv(l, 2);
+        // for(int i =0; i<=n;i++){
+        //     temp = red[i];
+        //     red[i]= red[l-i-1];
+        //     red[l-i-1] = temp;
 
+        // }
+        // for(int element: red){
+        //     System.out.print(element+" ");
+        // }
+
+        int max=0;
+        int min=0;
+        for(int e:red){
+            if(e>max){
+                max=e;
+            }
+            else if(e<min){
+                min=e;
+            }
         }
-        for(int element: red){
-            System.out.print(element+" ");
-        }
+        System.out.println(min);
+        System.out.println(max);
     }}
