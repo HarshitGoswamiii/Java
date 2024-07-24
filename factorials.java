5791eb4d2e2c216f6ae458377ddc552a9fc3a4ad
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-24 20:58:59 +0530

    commit

--- factorials.java
+++ factorials.java
@@ -1,11 +1,16 @@
 public class factorials {
-    static void table(int n){
-        for(int i = 0; i<=10;i++){
-            System.out.println(n+" * "+i+" = "+(n*i));
+    // static void table(int n){
+    //     for(int i = 1; i<=10;i++){
+    //         System.out.println(n+" * "+i+" = "+(n*i));
+    //     }
+    // }
+    static void pattern(int n){
+        for(int i=0;i<=n;i++){
+            
         }
     }
 
     public static void main(String[] args) {
-        table(5);
+        // table(5);
     }
 }
