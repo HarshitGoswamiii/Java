872c6a85c1bcb9636523b74d149f8fdb8637efbd
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-26 19:51:09 +0530

    commit

--- Practice/practise8.java
+++ Practice/practise8.java
@@ -1,27 +1,33 @@
-class Employee{
+class Employee { // Practice 1
     int salary;
     String name;
-    public void getsalary(){
-        salary = salary*1000;
+
+    public void getsalary() {
+        salary = salary * 1000;
         System.out.println(salary);
     }
-    public void getname(){
+
+    public void getname() {
         System.out.println(name);
     }
-    public void setname(String n){
+
+    public void setname(String n) {
         name = n;
     }
 }
 
-class square{
+class square { // Practice 3
     int side;
-    public void parameter(){
-        System.out.println("Parameter : "+(side*4));
+
+    public void parameter() {
+        System.out.println("Parameter : " + (side * 4));
     }
-    public void area(){
-        System.out.println("Area : "+(side*side));
+
+    public void area() {
+        System.out.println("Area : " + (side * side));
     }
 }
+
 public class practise8 {
     public static void main(String[] args) {
 
@@ -32,10 +38,13 @@ public class practise8 {
         // harshit.getname();
         // harshit.getsalary();
 
-        // Practice 2 ---------->>
+        // Practice 3 ---------->>
         square a = new square();
         a.side = 5;
-        a.area();
-        a.parameter();;
+        // a.area();
+        // a.parameter();
+
+        // Practice 4 --------->>
+
     }
 }
