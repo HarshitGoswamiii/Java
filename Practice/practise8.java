12bdaa3c853740ccea3ceb51b81053f21497c910
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-26 18:59:28 +0530

    commit

---
+++ Practice/practise8.java
@@ -0,0 +1,15 @@
+class Employee{
+    int salary;
+    int name;
+    public void getsalary(){
+        System.out.println(salary);
+    }
+    public void getname(){
+        
+    }
+}
+public class practise8 {
+    public static void main(String[] args) {
+        
+    }
+}
