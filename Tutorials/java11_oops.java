9875c5d99c40aeb17491a7ddb7a7a35dd42a5970
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-26 18:15:37 +0530

    commit

--- Tutorials/java11_oops.java
+++ Tutorials/java11_oops.java
@@ -1,8 +1,11 @@
 public class java11_oops {
-    public class details{
-        
+    class details{
+        int roll_no;
+        String name;
     }
     public static void main(String[] args){
+        details harshit = new details();
+        harshit.roll_no=19;
 
     }
 }
