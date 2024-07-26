63988ce5c310e67458f7ed1612a04b0be03008cd
Harshit Goswami <harshitgoswami244@gmail.com>
2024-07-26 18:34:07 +0530

    commit

--- Tutorials/java11_oops.java
+++ Tutorials/java11_oops.java
@@ -1,13 +1,19 @@
 class details{
     int roll_no;
     String name;
+    public void printdetail(){
+        System.out.println(roll_no);
+        System.out.println(name);
+    }
 }
 public class java11_oops {
     public static void main(String[] args){
         details harshit = new details();
         harshit.roll_no=19;
         harshit.name="Harshit Goswami";
-        System.out.println(harshit.roll_no);
-        System.out.println(harshit.name);
+        harshit.printdetail();
+        // System.out.println(harshit.roll_no);
+        // System.out.println(harshit.name);
+
     }
 }
