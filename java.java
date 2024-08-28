b9d66c01ad0ed01f6420ad494d8aa516c0ced4b4
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-28 19:29:49 +0530

    commit

--- java.java
+++ java.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 public class java {
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the Number : ");
-        char a = s.next().charAt(0);
-        if(a>='A' && a<='Z'){
-            System.out.println("Positive !");
-        }
-        else{
-            System.out.println("Negative !");
-        }
+
     }
 }
