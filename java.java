514d11a52c669ce6a9905271eb12e0f0cbbd2525
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-28 19:22:28 +0530

    commi

---
+++ java.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class java {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int a = s.nextInt();
+        
+    }
+}
