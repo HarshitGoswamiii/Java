8a637cf27f898a23539ed05191dc12806d1e7b6d
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-28 19:34:14 +0530

    commit

--- java.java
+++ java.java
@@ -1,9 +1,9 @@
 // import java.util.Scanner;
 public class java {
     public static void main(String[] args){
-        int a = 2;
-        for(int i = 1; i<=10;i++){
-            System.out.println(i*a);
+        // int a = 2;
+        for(char i = 'A'; i<='Z';i++){
+            System.out.println(i);
         }
     }
 }
