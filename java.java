42188715c36adcece6d1ec228de30c14484fa54f
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-28 19:39:49 +0530

    commti

--- java.java
+++ java.java
@@ -2,6 +2,7 @@
 public class java {
     public static void main(String[] args){
         int a = 2321;
+        int rev = 0;
         for( int i=0 ; i>a ; i--){
             System.out.println(i);
         }
