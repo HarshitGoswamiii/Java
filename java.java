4e2c106b0ffc4b75defe7e328abd3386dfc94f77
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 19:29:03 +0530

    commit

--- java.java
+++ java.java
@@ -1,12 +1,6 @@
 // import java.util.Scanner;
 public class java {
     public static void main(String[] args){
-        int a = 2321;
-        int rev = 0;
-            for(a; a!0;a/10){
-                int d= a%10;
-                rev= rev*10+d;
-            }
-            System.out.println("Reverse : "+rev);
+        
         }
     }
