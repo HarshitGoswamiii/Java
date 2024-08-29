33080b215fab8163ee1f37c2ca6bb4c5e33a546a
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 19:52:25 +0530

    commit

--- java.java
+++ java.java
@@ -1,17 +1,7 @@
 
-import java.util.Scanner;
 
 public class java {
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter Number : ");
-        int a = s.nextInt();
-        System.out.println(a);
-        if(a%2==0){
-            System.out.println("Even !");
-        }
-        else{
-            System.out.println("Odd !");
-        }
+        
         }
     }
