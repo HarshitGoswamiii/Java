ed87b8e120677ab290cd74986c35017d1384f2f6
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 19:42:04 +0530

    commit

--- java.java
+++ java.java
@@ -1,10 +1,6 @@
 // import java.util.Scanner;
 public class java {
     public static void main(String[] args){
-        int a=12,b=13;
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println("Swapped Value : "+a+" : "+b);
+        
         }
     }
