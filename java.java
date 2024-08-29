a8f7105d6d8a5763ef9db9e63b1604e224b24ebb
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 19:40:45 +0530

    commot

--- java.java
+++ java.java
@@ -1,6 +1,10 @@
 // import java.util.Scanner;
 public class java {
     public static void main(String[] args){
+        int a=12,b=13;
+        int temp=a;
+        a=b;
+        b=temp;
         
         }
     }
