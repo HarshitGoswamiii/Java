1b2aadc0e154f8117c3a202028fc10eaa538a434
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 20:07:02 +0530

    commit

--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 public class java {
     public static void main(String[] args){
-            int n=1;
+            int n=0;
             int n1=10;
             for(int i=0;i<=n1;i++){
                 n+=i;
