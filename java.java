8a2b7b8cd761cbc1c0e464e5645fa4fc178d75cf
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-29 19:44:57 +0530

    commit

--- java.java
+++ java.java
@@ -1,6 +1,10 @@
-// import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class java {
     public static void main(String[] args){
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = s.nextInt();
         }
     }
