d274c3b92adbebd47351c0e5fc18f388d70b6871
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 20:56:49 +0530

    commti

--- jva.java
+++ jva.java
@@ -1,7 +1,12 @@
 
 public class jva {
     static int fac(int num){
-        return num = num-fac(num-1);
+        if(num>=1){
+            return num = num*fac(num-1);
+        }
+        else{
+            return 1;
+        }
         }
     
     public static void main(String[] args) {
