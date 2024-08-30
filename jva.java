502e6c8d91e8088b2849d4bb0fc7338e329dbff7
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 20:34:24 +0530

    commit

--- jva.java
+++ jva.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
+
 public class jva {
     public static void main(String[] args) {
-     Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the First Number");
         int first = s.nextInt();
+        System.out.println("Enter the First Number");
         int second = s.nextInt();
         System.out.println("Select Your Operator : \n 1 : Addition\n 2 : Subtraction \n 3 : Multiply \n 4 : Divide");
         int calc = s.next().charAt(0);
         switch (calc) {
-            case '1' :
-                System.out.println(first+second);
+            case '1':
+                System.out.println(first + second);
                 break;
-            case '2' : 
-                System.out.println(first-second);
+            case '2':
+                System.out.println(first - second);
                 break;
-            case '3' : 
-                System.out.println(first*second);
+            case '3':
+                System.out.println(first * second);
                 break;
-            case '4' : 
-                System.out.println(first/second);
+            case '4':
+                System.out.println(first / second);
                 break;
             default:
-                System.out.println("Invalid OPerator ");;
-        }
+                System.out.println("Invalid OPerator ");
+                ;
         }
     }
+}
