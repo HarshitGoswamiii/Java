85942e39e7f46d1a23dcf6bee5edc97cbaad4d59
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 17:56:27 +0530

    commit

--- java.java
+++ java.java
@@ -1,11 +1,5 @@
 public class java {
     public static void main(String[] args){
-        // Palindrome 
-        int a1=12321, a2=0;
-        while(a1!=0){
-            a1/=10;
-            a2++;
-        }
-        System.out.println(a2);
+       
     }
     }
