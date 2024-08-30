320e1949c4b2ae8e8107c699aacfa677e267ece6
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 20:39:22 +0530

    commit

--- jva.java
+++ jva.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class jva {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter the First Number");
+        System.out.print("Enter the First Number : ");
         int first = s.nextInt();
-        System.out.println("Enter the First Number");
+        System.out.print("Enter the Second Number : ");
         int second = s.nextInt();
         System.out.println("Select Your Operator : \n 1 : Addition\n 2 : Subtraction \n 3 : Multiply \n 4 : Divide");
+        System.out.print("Enter Your Choice : ");
         int calc = s.next().charAt(0);
         switch (calc) {
             case '1':
