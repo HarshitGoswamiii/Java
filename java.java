39b474bef272a125d84ee547be93ef7b4a008e53
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 18:20:15 +0530

    commit

--- java.java
+++ java.java
@@ -1,13 +1,12 @@
 public class java {
     public static void main(String[] args) {
         int a =10,n1=0,n2=1;
-        System.out.print("Fibonicci Series until "+a+" : ");
-        int next = n1+n2;
-        n1=n2;
-        n2=next;
-        System.out.println(next);
+        System.out.println("Fibonicci Series until "+a+" : ");
         for(int i=1;i<=a;++i){
-            System.out.print(", "+next);
+            System.out.print(n1+", ");
+            int next = n1+n2;
+            n1=n2;
+            n2=next;
         }
     }
 }
