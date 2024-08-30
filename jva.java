48ed3e0373109c64bdcd8da8769a896a58ba5000
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 18:38:02 +0530

    commit

--- jva.java
+++ jva.java
@@ -1,8 +1,10 @@
 public class jva {
     public static void main(String[] args) {
-     int num = 10;
-     for (int i = 0; i<=num; i++) {
-         
+     int num = 60;
+     for (int i = 1; i<=num; i++) {
+        if(num%i==0){
+            System.out.print(i+", ");
+        }
      }
     }
 }
