de530f0aa3c53edd71cd279823c590dd1ffc4272
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 17:52:38 +0530

    git

--- java.java
+++ java.java
@@ -1,10 +1,10 @@
 public class java {
     public static void main(String[] args){
-            int n=0;
-            int n1=10;
-            for(int i=0;i<=n1;i++){
-                n+=i;
-                System.out.print(n+" ,");
-            }
+        // Palindrome 
+        int a1=12321, a2=0;
+        while(a1!=0){
+            a1/=10;
+            a2++;
         }
+    }
     }
