9b83c6dc3516d678efcf5d7cd74a5efd257aa2a8
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 18:51:08 +0530

    commit

--- jva.java
+++ jva.java
@@ -1,11 +1,28 @@
+import java.awt.im.InputContext;
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
 public class jva {
     public static void main(String[] args) {
-     int num = 60;
-     System.out.println("Factors of "+num+" : ");
-     for (int i = 1; i<=num; i++) {
-        if(num%i==0){
-            System.out.print(i+", ");
+     Scanner s = new Scanner(System.in);
+        int first = s.nextInt();
+        int second = s.nextInt();
+        System.out.println("Select Your Operator : \n 1 : Addition\n 2 : Subtraction \n 3 : Multiply \n 4 : Divide");
+        int calc = s.next().charAt(0);
+        switch (calc) {
+            case 1 :
+                System.out.println(first+second);
+                break;
+            case 2 : 
+                System.out.println(first-second);
+                break;
+            case 3 : 
+                System.out.println(first*second);
+                break;
+            case 4 : 
+                System.out.println(first/second);
+                break;
+            default:
+                System.out.println("Invalid OPerator ! ");;
         }
-     }
+        }
     }
-}
