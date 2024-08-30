86827d297c11874a78670b7c2b4e55cd316d5ca3
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 18:39:23 +0530

    commit

--- jva.java
+++ jva.java
@@ -1,6 +1,7 @@
 public class jva {
     public static void main(String[] args) {
      int num = 60;
+     System.out.println("Factors of "+num+" : ");
      for (int i = 1; i<=num; i++) {
         if(num%i==0){
             System.out.print(i+", ");
