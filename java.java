a2f27b4c2ae41b57ae13012dd9ce73af2252b178
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-30 17:58:52 +0530

    commit

--- java.java
+++ java.java
@@ -1,5 +1,7 @@
 public class java {
-    public static void main(String[] args){
-       
+    public static void main(String[] args) {
+        float a = 2.4f;
+        float b = 3.4f;
+        System.out.println(a*b);
     }
-    }
+}
