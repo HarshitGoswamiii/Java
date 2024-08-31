b6909d41ff66f29e3b1e711f6e2bd16c76989c24
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 19:06:19 +0530

    commit

--- array1.java
+++ array1.java
@@ -1,12 +1,19 @@
 public class array1 {
     public static void main(String[] args){
-        int[] arr={10,22,35,25,13,43};
-        int sum = 0;
-        int avg=arr.length;
-        for(int i = 0;i<arr.length;i++){
-            sum += arr[i];
+        int[] arr={10,90,35,49,13,43};
+        int largest = 0;
+        for(int i = 0 ; i<arr.length;i++){
+            if(largest<arr[i]){
+                largest = arr[i];
+            }
         }
-        System.out.println("Sum : "+sum);
-        System.out.println("Avg : "+sum/avg);
+        System.out.println("Largest number : "+largest);
+        // int sum = 0;
+        // int avg=arr.length;
+        // for(int i = 0;i<arr.length;i++){
+        //     sum += arr[i];
+        // }
+        // System.out.println("Sum : "+sum);
+        // System.out.println("Avg : "+sum/avg);
     }
 }
