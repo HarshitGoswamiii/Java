08a13532806aaa85c738e4dbbd69a63e0944e413
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 18:45:40 +0530

    commit

--- power.java
+++ power.java
@@ -1,14 +1,14 @@
 public class power {
-    static int powercalc(int base, int powerraised){
-        if(powerraised !=0){
-            return (base*powercalc(base, powerraised-1));
-        }
-        else{
+    static int powercalc(int base, int powerraised) {
+        if (powerraised != 0) {
+            return (base * powercalc(base, powerraised - 1));
+        } else {
             return 1;
         }
     }
-    public static void main(String[] args){
-        int base1 = 3, powerraised1=4;
+
+    public static void main(String[] args) {
+        int base1 = 3, powerraised1 = 4;
         System.out.println(powercalc(base1, powerraised1));
     }
 }
