3b8bb0a87ad6f044a96f801e8e674d5b9e40f74c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 18:53:28 +0530

    commit

--- array1.java
+++ array1.java
@@ -2,8 +2,8 @@ public class array1 {
     public static void main(String[] args){
         int[] arr={10,22,35,25,13,43};
         int sum = 0;
-        for(int i = 0;i<=arr.length;i++){
-            sum += i;
+        for(int i = 1;i<=arr.length;i++){
+            sum += arr[i];
         }
         System.out.println(sum);
     }
