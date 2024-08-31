e8e6352b1dcd789aa74110271d595511ba5d13e1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 19:42:14 +0530

    commit

--- alphabet.java
+++ alphabet.java
@@ -6,8 +6,21 @@ public class alphabet {
         for(int i=0; i<str1.length();i++){
             char ch=str1.charAt(i);
             if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-
+                vowels++;
+            }
+            else if(ch>='a' && ch<='z'){
+                consonant++;
+            }
+            else if(ch>='0' && ch<='9'){
+                digit++;
+            }
+            else{
+                blank++;
             }
         }
+        System.out.println("Vowels : "+vowels);
+        System.out.println("Consonant : "+consonant);
+        System.out.println("Digits : "+digit);
+        System.out.println("Blanks : "+blank);
     }
 }
