a2055d31a55188b570dab6a9bdaeea3700a49287
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 20:15:01 +0530

    commit

---
+++ dictionary.java
@@ -0,0 +1,6 @@
+public class dictionary {
+    public static void main(String[] args){
+        String[] Words={"Harshit","Aman","Ram","Izabella"};
+        for(int)
+    }
+}
---
+++ logic.java
@@ -0,0 +1,10 @@
+public class logic {
+    public static void main(String[] args) {
+        int last =100;
+        for(int i = 1; i<=last;i++){
+            for(int j = 1; j<=i;j++){
+                
+            }
+        }
+    }
+}
