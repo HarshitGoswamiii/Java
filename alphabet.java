baef05a4f2381d276cb851099447c35cb1148f3c
Harshit Goswami <harshitgoswami244@gmail.com>
2024-08-31 19:37:51 +0530

    commit

---
+++ alphabet.java
@@ -0,0 +1,13 @@
+public class alphabet {
+    public static void main(String[] args) {
+        String str = "this Website 143 Is Awesome 12";
+        String str1=str.toLowerCase();
+        int vowels = 0, consonant = 0, digit = 0, blank = 0;
+        for(int i=0; i<str1.length();i++){
+            char ch=str1.charAt(i);
+            if(ch=='a'|| ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+
+            }
+        }
+    }
+}
--- array2.java
+++ array2.java
@@ -1,11 +1,11 @@
 public class array2 {
     public static void main(String[] args){
-        String str = "Harshit Goswami";
+        String str = "Harshit Goswamiiii";
         int repeat = 0;
         char ch='i';
         for(int i = 0 ; i<str.length();i++){
             if(ch == str.charAt(i)){
-                repeat +=i;
+                repeat ++;
             }
         }
         System.out.println("Frequency : "+repeat);
