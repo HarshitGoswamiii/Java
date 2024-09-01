0c04f30ee4828232df73218b4b4fe61969a30a7b
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-01 20:34:30 +0530

    commit

--- logic.java
+++ logic.java
@@ -1,13 +1,15 @@
 public class logic {
     public static void main(String[] args) {
-        int last =4;
-        int first=1;
-        for(int i = 1; i<=last;i++){
-            for(int j = 1; j<=i;j++){
-                System.out.print(first*first + " ");
+        int last = 4;
+        int first = 1;
+        for (int i = 1; i <= last; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(first * first + " ");
                 first++;
             }
             System.out.print("\n");
         }
+
     }
+    
 }
