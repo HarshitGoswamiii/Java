2c8047015a9160c546a0bde8e504e27e322ce8e4
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:37:30 +0530

    commit

--- lcm.java
+++ lcm.java
@@ -2,10 +2,12 @@ public class lcm {
     public static void main(String[] args) {
         int[] arr={10,29,33,21};
         int[] arr2={20,43,22,12};
-        for(int i=1;i<arr.length;i++){
-            for(int j=1;j<arr2.length;j++){
-                System.out.println(arr[i]+arr2[j]);
+        int[] sum = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+                sum[i]=arr[i]+arr2[i];
             }
+        for (int num : sum) {
+            System.out.print(num+" ");
+        }
         }
     }
-}
