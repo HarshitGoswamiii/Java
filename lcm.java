4e7b7cdf61cad8459c38166f4ef7351ab6f16b19
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:29:01 +0530

    commit

--- lcm.java
+++ lcm.java
@@ -4,7 +4,7 @@ public class lcm {
         int[] arr2={20,43,22,12};
         for(int i=1;i<arr.length;i++){
             for(int j=1;j<arr2.length;j++){
-                System.out.println(arr[0]+arr2[0]);
+                System.out.println(arr[i]+arr2[j]);
             }
         }
     }
