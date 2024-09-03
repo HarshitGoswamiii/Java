7b0b31188f7348d4eef936c36797333053636f7d
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:48:13 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -1,8 +1,14 @@
 public class pattern {
     public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            for(int j=0;j<i;j++){
+        for (int i = 4; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
+                for (int k = 0; k < j; k++) {
+                    for (int l = 0; l < k; l++) {
+                        System.out.print("*");
+                    }
+                    // System.out.print("\n");
+                }
             }
             System.out.print("\n");
         }
