54c9423ba3fb187e98a7f9f684cd00ada05026bd
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:14:38 +0530

    commit

--- gcd.java
+++ gcd.java
@@ -3,7 +3,10 @@ public class gcd {
         int num1=86,num2=172;
         int GCD = 1;
         for(int i=0; i<= num1 && i<=num2;i++){
-            
+            if(num1%i==0 && num2%i==0){
+                GCD=i;
+            }
         }
+        System.out.println("GCD of "+num1+" And "+num2+" : "+GCD);
     }
 }
