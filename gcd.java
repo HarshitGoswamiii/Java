113ddef06f29f8ccc1d6b898c56ee99f1fe3183d
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:18:27 +0530

    commit

--- gcd.java
+++ gcd.java
@@ -1,12 +1,12 @@
 public class gcd {
-    public static void main(String[] args){
-        int num1=86,num2=172;
+    public static void main(String[] args) {
+        int num1 = 81, num2 = 153;
         int GCD = 1;
-        for(int i=0; i<= num1 && i<=num2;i++){
-            if(num1%i==0 && num2%i==0){
-                GCD=i;
+        for (int i = 1; i <= num1 && i <= num2; ++i) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                GCD = i;
             }
         }
-        System.out.println("GCD of "+num1+" And "+num2+" : "+GCD);
+        System.out.println("GCD of " + num1 + " And " + num2 + " : " + GCD);
     }
 }
