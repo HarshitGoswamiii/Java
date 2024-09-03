2fe882c6086485fa8b01b752918672e6d80c84aa
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 18:11:53 +0530

    commit

---
+++ gcd.java
@@ -0,0 +1,9 @@
+public class gcd {
+    public static void main(String[] args){
+        int num1=86,num2=172;
+        int GCD = 1;
+        for(int i=0; i<= num1 && i<=num2;i++){
+            
+        }
+    }
+}
