7f2f15aa2be98a03cb645e37430faabe85e686e6
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 21:17:30 +0530

    commmit

--- palindrome.java
+++ palindrome.java
@@ -1,10 +1,19 @@
+
+import java.util.stream.Stream;
+
 public class palindrome {
     public static void main(String[] args){
         String spell = "Radar";
         String rev="";
-        for(int i=(spell.length()-1) ;i>=0;i++){
+        for(int i=(spell.length()-1) ;i>=0;--i){
             rev = rev+spell.charAt(i);
         }
         System.out.print(rev);
+        if(rev.toLowerCase()==spell.toLowerCase()){
+            System.out.println("This String is Palindrome String !");
+        }
+        else{
+            System.out.println("This String is not a Palindrome String !");
+        }
     }
 }
