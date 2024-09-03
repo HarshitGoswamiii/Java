13ddc2b11e3ca8537e922ffaec4b3a5bde030013
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 21:00:22 +0530

    commit

--- integer.java
+++ integer.java
@@ -1,9 +1,9 @@
 public class integer {
     public static void main(String[] args) {
         int num = 3, power = 4;
-        int result = 0;
-        for (int i = 0; i <= power; i++) {
-            result = num * i;
+        int result = 1;
+        for (int i = 0;i<=power;i++) {
+            result = num * (power-i);
         }
         System.out.print(num + "^" + power + "=" + result);
     }
