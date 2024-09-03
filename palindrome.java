9c85d3d502c29b63fd076b5bba20bfbd1bbdc994
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-03 21:10:48 +0530

    commit

---
+++ palindrome.java
@@ -0,0 +1,9 @@
+public class palindrome {
+    public static void main(String[] args){
+        String spell = "Radar";
+        String rev="";
+        for(int i = 0 ;i<=spell.length();i++){
+            
+        }
+    }
+}
