90468a0f281f8fdb2561d72f80ce9efd5da31b04
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-04 18:48:28 +0530

    commit

--- prime.java
+++ prime.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 public class prime {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter Your Number : ");
+        System.out.print("Enter Starting Point : ");
         int num = s.nextInt();
+        System.out.print("Enter End Point : ");
+        int last = s.nextInt();
         boolean flag = false;
-        for (int i = 2; i <= num/2; i++) {
-            if(num%i==0){
-                flag=true;
-                break;
+        while(num<last){
+            for (int i = 2; i <= num/2; i++) {
+                if(num%i==0){
+                    flag=true;
+                    break;
+                }
             }
         }
-        if (!flag) {
-            System.out.println("Prime Number ! ");
-        } else {
-            System.out.println("Not a Prime Number ! ");
+        if(!flag && num!=0 && num!=1){
+            System.out.println(num+" ");
+            ++num;
         }
+        // if (!flag) {
+        //     System.out.println("Prime Number ! ");
+        // } else {
+        //     System.out.println("Not a Prime Number ! ");
+        // }
     }
 }
