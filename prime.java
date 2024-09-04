cba70a1fbe6bc36e43351686c7822c469cba3fef
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-04 18:28:45 +0530

    commit

--- prime.java
+++ prime.java
@@ -6,7 +6,7 @@ public class prime {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter Your Number : ");
         int num = s.nextInt();
-        for (int i = 0; i <= num; i++) {
+        for (int i = 2; i <= num; i++) {
             if (num % i == 0) {
                 System.out.println("Prime Number ! ");
             } else {
