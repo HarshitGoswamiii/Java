38b1f56640024cd61450d2d6581ae064002ec2a4
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-04 19:44:30 +0530

    commit

--- Revision/Interval.java
+++ Revision/Interval.java
@@ -1,9 +1,9 @@
 public class Interval {
-    static boolean prime(int num){
-        boolean flag = false;
-        for(int i =2; i<=num/2;++i){
-            if(num%i==0){
-                flag=true;
+    static boolean prime(int n){
+        boolean flag = true;
+        for(int i =2; i<=n/2;++i){
+            if(n%i==0){
+                flag=false;
                 break;
             }
         }
@@ -14,7 +14,7 @@ public class Interval {
         while(num<last){
             if(prime(num)){
                 System.out.print(num+" ");
-                ++num;
+            ++num;
             }
         }
     }
